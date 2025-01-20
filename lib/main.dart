ff7de3de316d1c872f9255bbcc1a93df82679288
Ashlesh Pandey <ashlesh@shrigsolutions.com>
2025-01-20 13:10:08 +0545

    feat: rect drawing successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
+import 'dart:developer';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:single_image_detection/extension/global_key_extension.dart';
-import 'package:single_image_detection/proofball_bouindary_painter.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:image/image.dart' as img;
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+import 'package:single_image_detection/extension/global_key_extension.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,13 +40,11 @@ class ObjectDetectionPage extends StatefulWidget {
 class _ObjectDetectionPageState extends State<ObjectDetectionPage> {
   late Interpreter interpreter;
   bool isLoading = true;
-  List<Map<String, dynamic>> detections = [];
+  Detection? detection;
 
   final GlobalKey _imageKey = GlobalKey();
-  ImageProvider? imageProvider;
+  late ImageProvider imageProvider;
   late ImageStreamListener _imageListener;
-
-  // Offsets calculated after scaling API results to device ratio
 
   Rect? pballRect;
 
@@ -58,23 +59,40 @@ class _ObjectDetectionPageState extends State<ObjectDetectionPage> {
   double imageDeviceHeight = 0;
   double imageOriginalHeight = 0;
   double imageOriginalWidth = 0;
-  Size imagesize = Size.zero;
 
-  void calcImageScaleFactorsAndScaleOffsets(
-      {required double imageOriginalHeight,
-      required double imageOriginalWidth,
-      required Rect rect}) {
+  List input = [];
+  List output = [];
+
+  @override
+  void initState() {
+    super.initState();
+    imageProvider = const AssetImage("assets/images/proofball.png");
+    loadModel();
+    final ImageStream stream = imageProvider.resolve(ImageConfiguration.empty);
+    _imageListener = ImageStreamListener((info, __) {
+      setState(() {
+        isImageLoaded = true;
+      });
+      resizeImageToInput(image: info.image);
+      startRetryUntilSizeDetermined(info: info);
+    });
+    stream.addListener(_imageListener);
+  }
+
+  // Function to calculate the scale factors for scaling api points to relative device size
+  void calcImageScaleFactorsAndScaleOffsets({
+    required ImageInfo info,
+  }) {
     final imageBounds = _imageKey.globalPaintBounds(null);
     if (imageBounds == null) return;
+    imageDeviceWidth = imageBounds.size.width;
+    imageDeviceHeight = imageBounds.size.height;
 
-    if (isImageLoaded && !invalidSize()) {
-      scaleX = imageBounds.width / imageOriginalWidth;
-      scaleY = imageBounds.height / imageOriginalHeight;
-
-      pballRect = Rect.fromLTRB(rect.left * scaleX, rect.top * scaleY,
-          rect.right * scaleX, rect.bottom * scaleY);
-
-      setState(() {});
+    imageOriginalHeight = info.image.height.toDouble();
+    imageOriginalWidth = info.image.width.toDouble();
+    if (isImageLoaded && !isScaledToDevice && !invalidSize()) {
+      scaleX = imageDeviceWidth / imageOriginalWidth;
+      scaleY = imageDeviceHeight / imageOriginalHeight;
     }
   }
 
@@ -85,165 +103,116 @@ class _ObjectDetectionPageState extends State<ObjectDetectionPage> {
         imageOriginalWidth == 0;
   }
 
-  @override
-  void initState() {
-    super.initState();
+  // Function to scale api points to relative device size
+  void scaleApiOffsetsToDeviceSize() {}
 
-    loadModel();
+  void startRetryUntilSizeDetermined({
+    required ImageInfo info,
+  }) {
+    _retryTimer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      if (isScaledToDevice || currentRetry >= maxRetries) {
+        _retryTimer?.cancel();
+      } else {
+        calcImageScaleFactorsAndScaleOffsets(
+          info: info,
+        );
+        currentRetry++;
+      }
+    });
   }
 
-  void calculateScaledBoundingBox(List<double> bbox, Size originalSize) {
-    final RenderBox? renderBox =
-        _imageKey.currentContext?.findRenderObject() as RenderBox?;
-    if (renderBox == null) return;
-
-    final Size displaySize = renderBox.size;
-
-    // Calculate scale factors
-    double scaleX = displaySize.width / originalSize.width;
-    double scaleY = displaySize.height / originalSize.height;
-
-    // Use the smaller scale factor to maintain aspect ratio
-    double scale = scaleX < scaleY ? scaleX : scaleY;
-
-    // Calculate padding to center the image
-    double horizontalPadding =
-        (displaySize.width - (originalSize.width * scale)) / 2;
-    double verticalPadding =
-        (displaySize.height - (originalSize.height * scale)) / 2;
-
-    // Scale and translate the bounding box
-    double scaledX1 = (bbox[0] * scale) + horizontalPadding;
-    double scaledY1 = (bbox[1] * scale) + verticalPadding;
-    double scaledX2 = (bbox[2] * scale) + horizontalPadding;
-    double scaledY2 = (bbox[3] * scale) + verticalPadding;
-
-    setState(() {
-      pballRect = Rect.fromLTRB(scaledX1, scaledY1, scaledX2, scaledY2);
-    });
+  @override
+  void dispose() {
+    final ImageStream stream = imageProvider.resolve(ImageConfiguration.empty);
+    stream.removeListener(_imageListener);
+    interpreter.close();
+    super.dispose();
   }
 
   Future<void> loadModel() async {
     try {
-      // ignore: unnecessary_string_escapes
+      setState(() {
+        isLoading = true;
+      });
       interpreter =
           await Interpreter.fromAsset("assets/ml/fish_detection.tflite");
       setState(() {
         isLoading = false;
       });
     } catch (e) {
-      print('Error loading model: $e');
+      log('Error loading model: $e');
     }
   }
 
-  Future<void> processImage(Size size) async {
-    try {
-      final ByteData imageData =
-          await rootBundle.load('assets/images/proofball.png');
-      final Uint8List bytes = imageData.buffer.asUint8List(
-        imageData.offsetInBytes,
-        imageData.lengthInBytes,
-      );
-      final img.Image? originalImage = img.decodeImage(bytes);
+  Future<void> resizeImageToInput({
+    required ui.Image image,
+  }) async {
+    final ByteData? bytedata =
+        await image.toByteData(format: ui.ImageByteFormat.png);
+    if (bytedata == null) return;
+    final originalImage = img.decodeImage(
+      bytedata.buffer.asUint8List(
+        bytedata.offsetInBytes,
+        bytedata.lengthInBytes,
+      ),
+    );
 
-      if (originalImage == null) throw Exception('Failed to load image');
+    if (originalImage == null) throw Exception('Failed to load image');
 
-      final int originalWidth = originalImage.width;
-      final int originalHeight = originalImage.height;
-
-      var imageSize = Size(originalWidth.toDouble(), originalHeight.toDouble());
-
-      // Resize image to 640x640
-      final img.Image resizedImage = img.copyResize(
-        originalImage,
-        width: 640,
-        height: 640,
-        interpolation: img.Interpolation.cubic,
-      );
-
-      // Create input tensor
-      var input = List.generate(
-        1,
-        (index) => List.generate(
+    // Resize image to 640x640
+    final resizedImage = img.copyResize(
+      originalImage,
+      width: 640,
+      height: 640,
+      interpolation: img.Interpolation.linear,
+    ); // Create input tensor
+    input = List.generate(
+      1,
+      (index) => List.generate(
+        640,
+        (y) => List.generate(
           640,
-          (y) => List.generate(
-            640,
-            (x) => List.generate(
-              3,
-              (c) {
-                final pixel = resizedImage.getPixel(x, y);
-                return c == 0
-                    ? pixel.r.toDouble() / 255.0
-                    : c == 1
-                        ? pixel.g.toDouble() / 255.0
-                        : pixel.b.toDouble() / 255.0;
-              },
-            ),
+          (x) => List.generate(
+            3,
+            (c) {
+              final pixel = resizedImage.getPixel(x, y);
+              return c == 0
+                  ? pixel.r.toDouble() / 255.0
+                  : c == 1
+                      ? pixel.g.toDouble() / 255.0
+                      : pixel.b.toDouble() / 255.0;
+            },
           ),
+        ),
+      ),
+    );
+
+    output = List.filled(1 * 300 * 6, 0).reshape([1, 300, 6]);
+  }
+
+  Future<void> detect() async {
+    try {
+      interpreter.run(input, output);
+
+      final score = output[0][0][4] as double?;
+      if (score == null) return;
+      double x1 = output[0][0][0] * imageDeviceWidth;
+      double y1 = output[0][0][1] * imageDeviceHeight;
+      double x2 = output[0][0][2] * imageDeviceWidth;
+      double y2 = output[0][0][3] * imageDeviceHeight;
+
+      detection = Detection(
+        confidence: score,
+        rect: Rect.fromPoints(
+          Offset(x1, y1),
+          Offset(x2, y2),
         ),
       );
 
-      var output = List.filled(1 * 300 * 6, 0).reshape([1, 300, 6]);
-      interpreter.run(input, output);
-
-      List<Map<String, dynamic>> processedDetections = [];
-
-      for (var i = 0; i < 5; i++) {
-        final score = output[0][i][4];
-        if (score > 0.5) {
-          double x1 = output[0][i][0] * originalWidth;
-          double y1 = output[0][i][1] * originalHeight;
-          double x2 = output[0][i][2] * originalWidth;
-          double y2 = output[0][i][3] * originalHeight;
-
-          processedDetections.add({
-            'class_id': output[0][i][5].toInt(),
-            'confidence': score,
-            'bbox': [x1, y1, x2, y2],
-            'original_dimensions': {
-              'width': originalWidth,
-              'height': originalHeight,
-            }
-          });
-
-          // Calculate scaled bounding box for display
-          if (imageSize != null) {
-            calculateScaledBoundingBox([x1, y1, x2, y2], imageSize!);
-          }
-        }
-      }
-
-      setState(() {
-        detections = processedDetections;
-      });
-
-      await initializeImage(bytes);
+      setState(() {});
     } catch (e) {
-      print('Error processing image: $e');
-      setState(() {
-        detections = [];
-      });
+      log('Error processing image: $e');
     }
-  }
-
-  Future<void> initializeImage(Uint8List data) async {
-    imageProvider = MemoryImage(data);
-    if (imageProvider == null) return;
-    final ImageStream stream = imageProvider!.resolve(ImageConfiguration.empty);
-    _imageListener = ImageStreamListener((info, __) {
-      if (mounted) {
-        setState(() {
-          isImageLoaded = true;
-        });
-      }
-    });
-    stream.addListener(_imageListener);
-  }
-
-  @override
-  void dispose() {
-    interpreter.close();
-    super.dispose();
   }
 
   @override
@@ -253,92 +222,148 @@ class _ObjectDetectionPageState extends State<ObjectDetectionPage> {
         title: const Text('TFLite Object Detection'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (isLoading)
-              Center(child: const CircularProgressIndicator())
-            else
-              Expanded(
+        child: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // imageStack(1.0),
-
-                    Image.asset(
-                      'assets/images/proofball.png',
-                      height: 400,
-                      width: 400,
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Detections: ${detections.length}',
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: detections.length,
-                        itemBuilder: (context, index) {
-                          final detection = detections[index];
-                          return ListTile(
-                            title: Text(
-                              'Class ${detection['class_id']}',
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
+                    Container(
+                      color: Colors.black,
+                      width: double.infinity,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxHeight: MediaQuery.of(context).size.height * 0.5,
+                        ),
+                        child: Stack(
+                          children: [
+                            Image(
+                              key: _imageKey,
+                              image: imageProvider,
+                              fit: BoxFit.contain,
                             ),
-                            subtitle: Text(
-                                'Confidence: ${(detection['confidence'] * 100).toStringAsFixed(2)}%\n'
-                                'BBox: [${detection['bbox'].map((e) => e.toStringAsFixed(3)).join(", ")}]'),
-                          );
-                        },
+                            if (detection != null)
+                              BoundaryBoxBorder(
+                                rect: detection!.rect,
+                                borderColor: Colors.green,
+                                borderWidth: 3,
+                              ),
+                          ],
+                        ),
                       ),
                     ),
                     const SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: () =>
-                          processImage(MediaQuery.of(context).size),
+                      onPressed: () {
+                        detect();
+                      },
                       child: const Text('Detect Objects'),
                     ),
                     const SizedBox(height: 20),
                   ],
                 ),
               ),
-          ],
-        ),
       ),
     );
   }
+}
 
-  // Widget imageStack(double scale) {
-  //   return Offstage(
-  //     offstage: !isImageLoaded,
-  //     child: Stack(
-  //       alignment: Alignment.center,
-  //       children: [
-  //         Container(
-  //           constraints: BoxConstraints(
-  //             maxHeight: 0.6 * MediaQuery.of(context).size.height,
-  //             maxWidth: MediaQuery.of(context).size.width,
-  //           ),
-  //           child: Builder(
-  //             builder: (context) {
-  //               return Image(
-  //                 key: _imageKey,
-  //                 image: imageProvider!,
-  //                 loadingBuilder: (context, child, loadingProgress) {
-  //                   if (loadingProgress == null) return child;
+class Detection {
+  final double confidence;
+  final Rect rect;
+  Detection({
+    required this.confidence,
+    required this.rect,
+  });
+}
 
-  //                   return const CircularProgressIndicator();
-  //                 },
-  //                 alignment: Alignment.center,
-  //                 fit: BoxFit.contain,
-  //               );
-  //             },
-  //           ),
-  //         ),
-  //         if (pballRect != null) ProofBallBoundaryPainter(rect: pballRect!)
-  //       ],
-  //     ),
-  //   );
-  // }
+class BoundaryBoxBorder extends StatelessWidget {
+  final Rect rect;
+  final Color borderColor;
+  final double borderWidth;
+
+  const BoundaryBoxBorder({
+    super.key,
+    required this.rect,
+    required this.borderColor,
+    required this.borderWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fromRect(
+        rect: rect,
+        child: CustomPaint(
+          foregroundPainter: FishBoundaryBoxScannerBorderPainter(
+            borderColor,
+            borderWidth,
+          ),
+        ));
+  }
+}
+
+// Modified to use topLeft and bottomRight for rectangle
+class FishBoundaryBoxScannerBorderPainter extends CustomPainter {
+  final Color borderColor;
+  final double borderWidth;
+
+  FishBoundaryBoxScannerBorderPainter(this.borderColor, this.borderWidth);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    const width = 2.0;
+    const radius = 2.5;
+    const tRadius = 3 * radius;
+    final rect = Rect.fromLTWH(
+      width,
+      width,
+      size.width - 2 * width,
+      size.height - 2 * width,
+    );
+    final rrect = RRect.fromRectAndRadius(rect, const Radius.circular(radius));
+    const clippingRect0 = Rect.fromLTWH(
+      0,
+      0, // Adjust these values for desired gap
+      2.7 * tRadius,
+      tRadius, // Increase height for top gap
+    );
+
+    final clippingRect1 = Rect.fromLTWH(
+      size.width - 2.7 * tRadius, // Adjusted width for longer top left side
+      0,
+      2.7 * tRadius,
+      tRadius,
+    );
+    final clippingRect2 = Rect.fromLTWH(
+      0,
+      size.height - tRadius,
+      tRadius * 2.7,
+      tRadius,
+    );
+    final clippingRect3 = Rect.fromLTWH(
+      size.width - 2.7 * tRadius,
+      size.height - tRadius,
+      tRadius * 2.7,
+      tRadius,
+    );
+    final path = Path()
+      ..addRect(clippingRect0)
+      ..addRect(clippingRect1)
+      ..addRect(clippingRect2)
+      ..addRect(clippingRect3);
+
+    canvas.clipPath(path);
+    canvas.drawRRect(
+      rrect,
+      Paint()
+        ..color = borderColor
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = borderWidth,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
 }
